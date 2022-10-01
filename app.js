96646b475c2588e3540d16e94a5e9548f2f2876b
Henry <trustedward@gmail.com>
2022-10-01 19:42:37 +0100

    i added the express.static method

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
-
+var path = require('path');
+var {sendFile} = require('fs');
+/*
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.sendFile(path.resolve(__dirname, './index.html'), function(){
+        console.log('done');
+    });
+}); */
+app.use(express.static('./files'));
+app.all('*', function(req, res){
+    res.send('not found');
+    console.log('404');
 })
 app.listen(2000);
